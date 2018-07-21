d8394c12dd910209af5dd5972ada442b803e81a7
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-21 00:17:13 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,38 +11,44 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
-	# In this code, create slightly different (for required, permitted) than update.
-	# If both the same, it can be simplified (see below with helper method)
 	# def create
 	# 	@post = Post.new(params.require(:post).permit(:title, :description))
 	# 	@post.save
 	# 	redirect_to post_path(@post)
 	# end
-	# 
+	#
 	# def update
 	# 	@post = Post.find(params[:id])
 	#   @post.update(params.require(:post).permit(:title))
 	#   redirect_to post_path(@post)
 	# end
 	def create
-		@post = Post.new(post_params)
+	  @post = Post.new(post_params(:title, :description))
 	  @post.save
-		redirect_to post_path(@post)
-	end
-		 
-	def update
-	  @post = Post.find(params[:id])
-	  @post.update(post_params)
 	  redirect_to post_path(@post)
 	end
-	  
+	 
+	def update
+	  @post = Post.find(params[:id])
+	  @post.update(post_params(:title))
+	  redirect_to post_path(@post)
+	end
+	 
 	def edit
 	  @post = Post.find(params[:id])
 	end
 
 	private
-	 
-	def post_params
-	  params.require(:post).permit(:title, :description)
+  
+	# We pass the permitted fields in as *args;
+	# this keeps `post_params` pretty dry while
+	# still allowing slightly different behavior
+	# depending on the controller action
+	def post_params(*args)
+	  params.require(:post).permit(*args)
 	end
+	# Ex. explicit setting of fields
+	#def post_params
+	#  params.require(:post).permit(:title, :description)
+	#end
 end
