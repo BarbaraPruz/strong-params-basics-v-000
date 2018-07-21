8fe819ae4e12afd4c1e40ce408603deccff74ccd
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-21 00:14:06 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,19 +11,38 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
+	# In this code, create slightly different (for required, permitted) than update.
+	# If both the same, it can be simplified (see below with helper method)
+	# def create
+	# 	@post = Post.new(params.require(:post).permit(:title, :description))
+	# 	@post.save
+	# 	redirect_to post_path(@post)
+	# end
+	# 
+	# def update
+	# 	@post = Post.find(params[:id])
+	#   @post.update(params.require(:post).permit(:title))
+	#   redirect_to post_path(@post)
+	# end
 	def create
-		@post = Post.new(params.require(:post).permit(:title, :description))
-		@post.save
+		@post = Post.new(post_params)
+	  @post.save
 		redirect_to post_path(@post)
 	end
-
+		 
 	def update
-		@post = Post.find(params[:id])
-	  @post.update(params.require(:post).permit(:title))
+	  @post = Post.find(params[:id])
+	  @post.update(post_params)
 	  redirect_to post_path(@post)
 	end
-
+	  
 	def edit
 	  @post = Post.find(params[:id])
+	end
+
+	private
+	 
+	def post_params
+	  params.require(:post).permit(:title, :description)
 	end
 end
